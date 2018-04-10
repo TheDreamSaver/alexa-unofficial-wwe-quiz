07c66a16daf343afa4d7b3173e806ac029626fc1
Ashish Jha <ashishjhaofficial@outlook.com>
2018-04-11 02:25:08 +0530

    Create data.js

---
+++ src/data.js
@@ -0,0 +1,47 @@
+const OD = [
+  {
+    question: "Which out of the championships in WWE has John Cena never held?",
+    answer: "intercontinental championship",
+    finisher: "given an attitude adjustment by John Cena",   
+  },
+  {
+    question: "Which out of the championships in WWE has John Cena never held?",
+    answer: "intercontinental championship",
+    finisher: "given an attitude adjustment by John Cena",   
+  },
+  {
+    question: "Which out of the championships in WWE has John Cena never held?",
+    answer: "intercontinental championship",
+    finisher: "given an attitude adjustment by John Cena",   
+  },
+  {
+    question: "Which out of the championships in WWE has John Cena never held?",
+    answer: "intercontinental championship",
+    finisher: "given an attitude adjustment by John Cena",   
+  },
+  {
+    question: "Which out of the championships in WWE has John Cena never held?",
+    answer: "intercontinental championship",
+    finisher: "given an attitude adjustment by John Cena",   
+  },
+  {
+    question: "Which out of the championships in WWE has John Cena never held?",
+    answer: "intercontinental championship",
+    finisher: "given an attitude adjustment by John Cena",   
+  },
+  {
+    question: "Which out of the championships in WWE has John Cena never held?",
+    answer: "intercontinental championship",
+    finisher: "given an attitude adjustment by John Cena",   
+  },
+  {
+    question: "Which out of the championships in WWE has John Cena never held?",
+    answer: "intercontinental championship",
+    finisher: "given an attitude adjustment by John Cena",   
+  },
+];
+
+const AR = Array.from(Array(OD.length).keys());
+
+module.exports.OD = OD;
+module.exports.AR = AR;
