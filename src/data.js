e2fd9a3ea75e3d8b3aab31010601e3bd6892938c
Ashish Jha <ashishjhaofficial@outlook.com>
2018-04-11 03:07:52 +0530

    Update data.js

--- src/data.js
+++ src/data.js
@@ -2,43 +2,53 @@ const OD = [
   {
     question: "Which out of the championships in WWE has John Cena never held?",
     answer: "intercontinental championship",
-    finisher: "given an attitude adjustment by John Cena",   
+    finisher: "given an Attitude Adjustment by John Cena",   
   },
   {
-    question: "Which out of the championships in WWE has John Cena never held?",
-    answer: "intercontinental championship",
-    finisher: "given an attitude adjustment by John Cena",   
+    question: "Which former member of the shield was the first one to win the intercontinental championship?",
+    answer: "dean ambrose",
+    finisher: "given a Dirty Deeds by Dean Ambrose",   
   },
   {
-    question: "Which out of the championships in WWE has John Cena never held?",
-    answer: "intercontinental championship",
-    finisher: "given an attitude adjustment by John Cena",   
+    question: "Who was the very first Divas Champion, which she won back in 2008?",
+    answer: "michelle mccool",
+    finisher: "given a Faith Breaker by Michelle McCool",   
   },
   {
-    question: "Which out of the championships in WWE has John Cena never held?",
-    answer: "intercontinental championship",
-    finisher: "given an attitude adjustment by John Cena",   
+    question: "Which superstar is billed from the bottomless pit?",
+    answer: "the boogeyman",
+    finisher: "given a Boogey Slam by The Boogeyman",   
   },
   {
-    question: "Which out of the championships in WWE has John Cena never held?",
-    answer: "intercontinental championship",
-    finisher: "given an attitude adjustment by John Cena",   
+    question: "Which superstar won the inaugural Money in the Bank ladder match in 2005?",
+    answer: "edge",
+    finisher: "given a Spear by Edge",   
   },
   {
-    question: "Which out of the championships in WWE has John Cena never held?",
-    answer: "intercontinental championship",
-    finisher: "given an attitude adjustment by John Cena",   
+    question: "Who holds the record for the longest time in a Royal Rumble match?",
+    answer: "rey mysterio",
+    finisher: "given a 6-1-9 by Rey Mysterio",   
   },
   {
-    question: "Which out of the championships in WWE has John Cena never held?",
-    answer: "intercontinental championship",
-    finisher: "given an attitude adjustment by John Cena",   
+    question: "Which superstar won the inaugural Andre the Giant Memorial Battle Royal at WrestleMania 30?",
+    answer: "cesaro",
+    finisher: "given a Neutralizer by Cesaro",   
   },
   {
-    question: "Which out of the championships in WWE has John Cena never held?",
-    answer: "intercontinental championship",
-    finisher: "given an attitude adjustment by John Cena",   
+    question: "Which WWE Hall of Famer was sworn in as the new President of the United States of America?",
+    answer: "donald trump",
+    finisher: "given a Covfefe by Donald Trump",   
   },
+  {
+    question: "Who holds the record for most Royal Rumble eliminations?",
+    answer: "roman reigns",
+    finisher: "given a Superman Punch by Roman Reigns",   
+  },
+  {
+    question: "What rebellious group led by Wade Barrett caused havoc on Raw?",
+    answer: "nexus",
+    finisher: "given a Wasteland by Wade Barrett",   
+  }
 ];
 
 const AR = Array.from(Array(OD.length).keys());
